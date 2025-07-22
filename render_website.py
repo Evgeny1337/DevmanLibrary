ff3df618d8d6161d26ea2f0139fcadacbc829c82
Evgeny1337 <genya-sozykin@mal.ru>
2025-07-22 09:54:42 +0500

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -3,24 +3,35 @@ from more_itertools import chunked
 from urllib.parse import quote
 from livereload import Server
 import json
+import os
 
+PAGES_PATH = "pages"
 
-
-def on_reload():
+def create_page(page_number, books):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-    with open("meta_data.json", "r") as my_file:
-        books_json = my_file.read()
-
-    books = json.loads(books_json)
     rows_of_books = list(chunked(books, 2))
     template = env.get_template('template.html')
     rendered_page = template.render(rows_of_books=rows_of_books)
+    os.makedirs(PAGES_PATH, exist_ok=True)
+    file_name = '{}/index{}.html'.format(PAGES_PATH, page_number)
 
-    with open("index.html", "w", encoding='utf8') as my_file:
+    with open(file_name, "w", encoding='utf8') as my_file:
         my_file.write(rendered_page)
+
+def on_reload():
+    with open("meta_data.json", "r") as my_file:
+        books_json = my_file.read()
+    books = json.loads(books_json)
+    for book in books:
+        book['img_src'] = '../{}'.format(book['img_src'])
+        book['book_path'] = '../{}'.format(book['book_path'])
+    book_pages = list(chunked(books, 20))
+    for index,book_page in enumerate(book_pages):
+        create_page(index, book_page)
+
 
 def main():
     on_reload()
